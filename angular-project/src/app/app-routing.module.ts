29478930acb615c381b6eb93d2fbd1505e92061a
Victor Labussiere <victor.labussiere95@gmail.com>
2022-12-07 17:28:03 -0300

    start routes with angular and installed json-server for a mock database

--- angular-project/src/app/app-routing.module.ts
+++ angular-project/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './component/first-component/first-component.component';
+import { ListRenderComponent } from './component/list-render/list-render.component';
+import { TwoWayBindingComponent } from './component/two-way-binding/two-way-binding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'lists', component: ListRenderComponent },
+  { path: 'two-way-binding', component: TwoWayBindingComponent }
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ChangeNumberComponent } from './component/change-number/change-number.component';
 import { DiretivasComponent } from './component/diretivas/diretivas.component';
